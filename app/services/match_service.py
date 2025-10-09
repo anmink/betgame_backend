efedde8f8b994b50cdd116b95593d90f51927732
anmink <anne-maina@web.de>
2025-10-09 22:17:25 +0200

    clean code

--- app/services/match_service.py
+++ app/services/match_service.py
@@ -4,6 +4,7 @@ import json
 from typing import List
 from app.models.match import Match
 from app.db.supabase_client import supabase
+from fastapi import HTTPException
 
 API_KEY = os.getenv("API_FOOTBALL_KEY")
 BASE_URL = "https://v3.football.api-sports.io"
@@ -62,58 +63,11 @@ class MatchService:
                 )
                 matches.append(match) 
 
-            supabase.table("matches").delete().neq("fixture_id", 0).execute()
-            supabase.table("matches").insert([m.dict() for m in matches]).execute()
-
-
-    async def save_matches():
-        print("hi")
-        with open("fixtures.json", "r") as f:
-            fixtures = json.load(f)
-
-        with open("odds.json", "r") as f:
-            odds = json.load(f)
-
-        combined = []
-        odd_dict = {item["fixture"]["id"]: item for item in odds["response"]}
-
-        for fixture in fixtures["response"]:
-            fixture_id = fixture["fixture"]["id"]
-            if fixture_id in odd_dict:
-                merged = {**odd_dict[fixture_id], **fixture}
-            else:
-                merged = fixture
-            combined.append(merged)
-
-        with open("combined.json", "w") as f:
-            json.dump(combined, f)
-        
-        matches = []
-        for item in combined:
-            values = item.get("bookmakers", [{}])[0].get("bets", [{}])[0].get("values", [])
-            match = Match(
-                fixture_id=item.get("fixture", {}).get("id"),
-                fixture_date=item.get("fixture", {}).get("date"),
-                fixture_status=item.get("fixture", {}).get("status", {}).get("long"),
-                league_id=item.get("league", {}).get("id"),
-                league_name=item.get("league", {}).get("name"),
-                league_round=item.get("league", {}).get("round", "Unknown").split("-")[-1].strip(),
-                teamhome_name=item.get("teams", {}).get("home", {}).get("name"),
-                teamhome_logo=item.get("teams", {}).get("home", {}).get("logo"),
-                teamhome_winner=item.get("teams", {}).get("home", {}).get("winner"),
-                teamaway_name=item.get("teams", {}).get("away", {}).get("name"),
-                teamaway_logo=item.get("teams", {}).get("away", {}).get("logo"),
-                teamaway_winner=item.get("teams", {}).get("away", {}).get("winner"),
-                goal_home=item.get("goals", {}).get("home"),
-                goal_away=item.get("goals", {}).get("away"),
-                odd_home = float(values[0]["odd"]) if len(values) > 0 else None,
-                odd_draw = float(values[1]["odd"]) if len(values) > 0 else None,
-                odd_away = float(values[2]["odd"]) if len(values) > 0 else None,
-            )
-            matches.append(match)
-
-        """ with open("matches.json", "w") as f:
-            json.dump([m.dict() for m in matches], f, indent=2)   """   
-
-        supabase.table("matches").delete().neq("fixture_id", 0).execute()
-        supabase.table("matches").insert([m.dict() for m in matches]).execute()
+            try:
+                respone_delete = supabase.table("matches").delete().neq("fixture_id", 0).execute()
+                response_insert = supabase.table("matches").insert([m.dict() for m in matches]).execute()
+                return respone_delete, response_insert
+            except Exception as e:
+                raise HTTPException(status_code=500, detail=str(e))
+            
+            
