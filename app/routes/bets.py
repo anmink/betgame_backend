a17b56843a9d62f8e2ba8fc7065f88601c7522f9
anmink <anne-maina@web.de>
2025-11-02 16:13:29 +0100

    add post bet endpoint

--- app/routes/bets.py
+++ app/routes/bets.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
+from app.middleware.auth_dependency import get_current_user
 from app.services.bet_service import BetService
 from app.models.bet import Bet
+
 
 router = APIRouter(prefix="/bets", tags=["bets"])
 
@@ -29,14 +31,24 @@ async def place_bet(user_id: str):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e)) """
     
-@router.post("/")
+""" @router.post("/")
 async def place_bet(bet: Bet):
     try:
         #user_id = "f17e2687-487f-48a9-a1d0-34cb370f68ec"
         response = await BetService.place_bet(bet=bet, user_id=user_id)
         return response
     except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e)) """
+
+@router.post("/")
+async def place_bet(bet: Bet, current_user: dict = Depends(get_current_user)):
+    try:
+        user_id = current_user["id"]
+        response = await BetService.place_bet(bet, user_id)
+        return response
+    except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
     
 @router.delete("/")
 async def delete_bet(bet_id: str, user_id: str):
